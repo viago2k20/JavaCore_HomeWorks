e3d630bd55706f7e77c8325300bd00d9fbc32159
Denis <denzh@mail.ru>
2023-11-12 15:58:04 +0300

    modified_0 InputOutputStreams_Task1

--- src/InputOutputStreams_Task1/Main.java
+++ src/InputOutputStreams_Task1/Main.java
@@ -9,87 +9,86 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
+
+    public static void deleteDirectory(File directory) {
+        File[] contents = directory.listFiles();
+        if (contents != null) {
+            for (File file : contents) {
+                deleteDirectory(file);
+            }
+        }
+        directory.delete();
+    }
+
     public static void main(String[] args) {
+
+        File directory = new File("Games");
+        deleteDirectory(directory);
+
         StringBuilder myLog = new StringBuilder();
 
-//        List<String> listDir = new ArrayList<>();
-//        listDir.add("Games");
-//        listDir.add("Games/src");
-//        listDir.add("Games/res");
-//        listDir.add("Games/savegames");
-//        listDir.add("Games/temp");
-//        listDir.add("Games/src/main");
-//        listDir.add("Games/src/test");
-//        listDir.add("Games/res/drawables");
-//        listDir.add("Games/res/vectors");
-//        listDir.add("Games/res/icons");
-//
-//        int count = 0;
-//        for (String dir : listDir) {
-//
-//            File file = new File(dir);
-////            file.delete();
-//
-//            if (file.mkdirs()) {
-//                count++;
-//                myLog.append(count).append(". ")
-//                        .append("Создание директории: ").append(dir).append(" ")
-//                        .append("\t").append(LocalDateTime.now())
-//                        .append("\n");
-//                System.out.println("Директория создана");
-//            } else {
-//                System.out.println("Директория не создана");
-//            }
-//        }
-//
-//        List<String> myFiles = new ArrayList<>();
-//        myFiles.add("Games/src/main/Main.java");
-//        myFiles.add("Games/src/main/Utils.java");
-//        myFiles.add("Games/temp/temp.txt");
-//
-//        for (String file : myFiles) {
-//            count++;
-//            File myFile = new File(file);
-////            myFile.delete();
-//
-//            try {
-//                if (myFile.createNewFile())
-//                    System.out.println("Файл был создан");
-//                myLog.append(count).append(". ")
-//                        .append("Файл был создан: ").append(file).append(" ")
-//                        .append("\t").append(LocalDateTime.now())
-//                        .append("\n");
-//            } catch (IOException ex) {
-//                System.out.println(ex.getMessage());
-//            }
-//        }
-//        System.out.println(myLog);
-//
-//        try (FileWriter writer = new FileWriter("Games/temp/temp.txt", false)) {
-//            writer.write(String.valueOf(myLog));
-//        } catch (IOException ex) {
-//            System.out.println(ex.getMessage());
-//        }
-//
+        List<String> listDir = new ArrayList<>();
+        listDir.add("Games");
+        listDir.add("Games/src");
+        listDir.add("Games/res");
+        listDir.add("Games/savegames");
+        listDir.add("Games/temp");
+        listDir.add("Games/src/main");
+        listDir.add("Games/src/test");
+        listDir.add("Games/res/drawables");
+        listDir.add("Games/res/vectors");
+        listDir.add("Games/res/icons");
+
+        int count = 0;
+        for (String dir : listDir) {
+            File file = new File(dir);
+            if (file.mkdirs()) {
+                count++;
+                myLog.append(count).append(". ")
+                        .append("Создание директории: ").append(dir).append(" ")
+                        .append("\t").append(LocalDateTime.now())
+                        .append("\n");
+                System.out.println("Директория создана");
+            } else {
+                System.out.println("Директория не создана");
+            }
+        }
+
+        List<String> myFiles = new ArrayList<>();
+        myFiles.add("Games/src/main/Main.java");
+        myFiles.add("Games/src/main/Utils.java");
+        myFiles.add("Games/temp/temp.txt");
+
+        for (String file : myFiles) {
+            count++;
+            File myFile = new File(file);
+            try {
+                if (myFile.createNewFile())
+                    System.out.println("Файл был создан");
+                myLog.append(count).append(". ")
+                        .append("Файл был создан: ").append(file).append(" ")
+                        .append("\t").append(LocalDateTime.now())
+                        .append("\n");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        System.out.println(myLog);
+
+        try (FileWriter writer = new FileWriter("Games/temp/temp.txt", false)) {
+            writer.write(String.valueOf(myLog));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
         GameProgress game1 = new GameProgress(120, 50, 10, 20);
         GameProgress game2 = new GameProgress(250, 280, 20, 250);
         GameProgress game3 = new GameProgress(3100, 310, 350, 3100);
-//        game1.saveGame("Games/savegames/saveGame1.dat", game1);
-//        game2.saveGame("Games/savegames/saveGame2.dat", game2);
-//        game3.saveGame("Games/savegames/saveGame3.dat", game3);
-//
-//        GameProgress game11 = null;
-//        GameProgress game21 = null;
-//        GameProgress game31 = null;
-//
-//        game11 = game1.loadGame("Games/savegames/saveGame1.dat");
-//        game21 = game2.loadGame("Games/savegames/saveGame2.dat");
-//        game31 = game3.loadGame("Games/savegames/saveGame3.dat");
-//
-//        System.out.println(game11);
-//        System.out.println(game21);
-//        System.out.println(game31);
-//
+
+        game1.saveGame("Games/savegames/saveGame1.dat", game1);
+        game2.saveGame("Games/savegames/saveGame2.dat", game2);
+        game3.saveGame("Games/savegames/saveGame3.dat", game3);
+
         String zFile = "Games/savegames/zip.zip";
         String file1 = "Games/savegames/saveGame1.dat";
         String file2 = "Games/savegames/saveGame2.dat";
@@ -100,40 +99,27 @@ public class Main {
         toZipList.add(file2);
         toZipList.add(file3);
 
-//        zipList(zFile, toZipList);
-
+        zipList(zFile, toZipList);
 
         for (String file : toZipList) {
             File zipFile = new File(file);
             zipFile.delete();
         }
 
-//        unzipFiles("Games/savegames", zFile);
+        unzipFiles("Games/savegames", zFile);
 
-//        File zipFile = new File("saveGame3.dat");
-//        zipFile.delete();
+        GameProgress game11 = null;
+        GameProgress game21 = null;
+        GameProgress game31 = null;
 
-//        unzipFiles(".", zFile);
+        game11 = game1.loadGame("Games/savegames/saveGame1.dat");
+        game21 = game2.loadGame("Games/savegames/saveGame2.dat");
+        game31 = game3.loadGame("Games/savegames/saveGame3.dat");
 
+        System.out.println(game11);
+        System.out.println(game21);
+        System.out.println(game31);
 
-    }
-
-
-    public static void zipFiles(String path, String file) {
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path));
-             FileInputStream fis = new FileInputStream(file)) {
-
-            ZipEntry entry = new ZipEntry("notes.txt");
-            zout.putNextEntry(entry);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-
-            zout.write(buffer);
-            zout.closeEntry();
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
     }
 
     public static void zipList(String path, List<String> files) {
@@ -155,7 +141,6 @@ public class Main {
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-
     }
 
     public static void unzipFiles(String where, String file) {
@@ -180,6 +165,4 @@ public class Main {
             System.out.println(ex.getMessage());
         }
     }
-
-
 }
